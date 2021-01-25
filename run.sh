acca21499ae95e0ee925ae9fcab7188c2d722c74
kornelro <kornel.ro@gmail.com>
2021-01-25 01:08:01 +0100

    helm

--- run.sh
+++ run.sh
@@ -4,9 +4,9 @@ POPULATION=$3
 NUM_SIMULATIONS=$4
 NUM_PROCESSES=$5
 
-python3 ./experiments/prepare_scenarios.py [decision_tree/pub_trans_comfort_dist-up,decision_tree/pub_trans_punctuality_dist-up,decision_tree/household_cars_dist-down] [0.,0.05,0.1,0.15] $SERVICES_NUMBER ./experiments/scenarios
+python3 ./experiments/prepare_scenarios.py [decision_tree/pub_trans_comfort_dist-up,decision_tree/pub_trans_punctuality_dist-up,decision_tree/household_cars_dist-down] [0.,0.15,0.3] $SERVICES_NUMBER ./experiments/scenarios
 
-# docker build . -t mobility-simulator
+docker build . -t mobility-simulator
 
 # for i in $(seq 1 $SERVICES_NUMBER);
 # do
